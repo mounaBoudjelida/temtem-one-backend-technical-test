2df578e8a45a38c7d2d6e1f096f701ed82c86c1d
mouna boudjelida <dm_boudjelida@esi.dz>
2024-06-26 22:34:35 +0100

    import loggerModule in emailsModule

--- src/modules/shared/emails/emails.module.ts
+++ src/modules/shared/emails/emails.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from './emails.service';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
+  imports: [LoggerModule],
   providers: [EmailsService],
   exports: [EmailsService],
 })
